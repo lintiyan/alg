dfc8b03fd9edb47213cf126f531f55efa1d9054c
lintiyan <2268313664@qq.com>
2022-08-21 11:58:01 +0800

    '寻找峰值'

--- 18.寻找峰值/main.go
+++ 18.寻找峰值/main.go
@@ -38,6 +38,7 @@ func findPeakElement(nums []int) int {
 		temp := (left + right) / 2
 		fmt.Println("temp=",temp, "left=", left, "right=", right)
 
+		// 如果temp元素大于它左右两边的元素，那么这个元素就是峰值
 		if ((temp+1 > len(nums) -1) || nums[temp] > nums[temp+1]) && ( temp-1 < 0 || nums[temp] > nums[temp -1]) {
 			return temp
 		}
@@ -53,10 +54,28 @@ func findPeakElement(nums []int) int {
 	return 0
 }
 
+func findPeakElement2(nums []int) int {
+	var left = 0
+	var right = len(nums) - 1
+
+	for left < right {
+		temp := (left + right) >> 1
+		fmt.Println("temp=",temp, "left=", left, "right=", right)
+
+		if nums[temp] < nums[temp+1] {
+			left = temp + 1
+		} else if nums[temp] > nums[temp+1] {
+			right = temp
+		}
+	}
+	return left
+}
+
 func main() {
 
-	//nums := []int{8,6,4,9,2,3,4,1,9,76,66,55,22}
+	nums := []int{8,6,4,9,2,3,4,1,9,76,66,55,22}
 	//nums := []int{1,2,1,3,5,6,4}
-	nums:= []int{11,2}
+	//nums:= []int{11,2}
 	fmt.Println(findPeakElement(nums))
+	fmt.Println(findPeakElement2(nums))
 }
